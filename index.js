051f4f45cd6cd08a65fcadc694263aebb35be1e2
Gustavo <gustavo.siqueira@brainweb.com.br>
2021-10-29 16:30:42 -0300

    Adiciona atualização e definição de tamanho para a ampulheta

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const runTimer = async (lines) => {
+  console.clear();
+
+  renderTable(lines);
+  setInterval(() => {
+    console.clear();
+
+    renderTable(lines);
+  }, 1000);
+};
+
 const renderTable = (maxLines) => {
   // criar um espaço da linha de comando
   console.log();
@@ -48,4 +66,39 @@ const renderCell = (line, maxLines) => {
   }
 };
 
-renderTable(21);
+const run = () => {
+  console.log("");
+  rl.question(
+    "  \033[1mBem vindo ao gerador de \033[34mAmpulheta\033[37m, qual o tamanho que você gostaria de ver? ",
+    (answer) => {
+      const lines = Number(answer);
+
+      // Validação do input
+      if (isNaN(lines)) {
+        console.log("");
+        console.log(
+          "  \033[0m\033[31mINPUT ERROR: insert a valid number\033[0m"
+        );
+
+        run();
+      }
+      // Validar caso o input seja menor que 20
+      else if (lines < 20) {
+        console.clear();
+        console.log("");
+        console.log(
+          "  Por favor insira um número maior que \033[33m20\033[0m para prosseguir"
+        );
+
+        // Pergunta novamente para o usuario
+        run();
+      }
+      // Cria a ampulheta caso satisfaça as condições
+      else {
+        runTimer(lines);
+      }
+    }
+  );
+};
+
+run();
